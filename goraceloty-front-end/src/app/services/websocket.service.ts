2d6e43ab42915612cbb833c80d7effcccb1008e4
MDG369 <michazieli@gmail.com>
2024-06-09 17:34:17 +0200

    authentication setup for endpoints and websockets

--- goraceloty-front-end/src/app/services/websocket.service.ts
+++ goraceloty-front-end/src/app/services/websocket.service.ts
@@ -4,6 +4,7 @@ import { eventsRxStompConfig } from "./rx-stomp-factory.service";
 import { Subject } from "rxjs";
 import { Injectable } from "@angular/core";
 import {ChangesMessage} from "../entity/ChangesMessage";
+import {AuthService} from "./auth.service";
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +15,7 @@ export class WebsocketService {
     return this.changesSubject.asObservable();
   }
 
-  constructor(private rxStompService: RxStompService,
+  constructor(private rxStompService: RxStompService, private authService: AuthService
               ) {
     this.rxStompService.watch('/topic/changes')
       .subscribe((message: any): void => {
@@ -31,16 +32,20 @@ export class WebsocketService {
   }
 
   public setupWebsocketEvents(): void {
-    const stompConfig: RxStompConfig = eventsRxStompConfig();
-    // @ts-ignore
-    this.rxStompService.configure(stompConfig);
-    this.rxStompService.activate();
+    if (this.authService.isLoggedIn()) {
+      const stompConfig: RxStompConfig = eventsRxStompConfig();
+      // @ts-ignore
+      this.rxStompService.configure(stompConfig);
+      this.rxStompService.activate();
+    }
   }
 
   public tearDownWebsocketEvents(): void {
     this.rxStompService.deactivate()
       .then(
-        () => {console.log('Event service disconnected'); this.setupWebsocketEvents()},
+        () => {console.log('Event service disconnected');
+          this.setupWebsocketEvents()
+        },
         (reason) => console.log(`Something bad happened during disconnect: ${reason}`));
   }
 }
