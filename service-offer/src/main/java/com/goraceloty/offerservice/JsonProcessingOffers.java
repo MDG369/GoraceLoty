327d7ae007da2aa4b56988ef98a3ed0a9ed8bf7d
Cimin123 <151384049+Cimin123@users.noreply.github.com>
2024-06-10 22:46:04 +0200

    Automatic offers inport

--- service-offer/src/main/java/com/goraceloty/offerservice/JsonProcessingOffers.java
+++ service-offer/src/main/java/com/goraceloty/offerservice/JsonProcessingOffers.java
@@ -1,42 +1,15 @@
 package com.goraceloty.offerservice;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.time.format.DateTimeFormatter;
 
 public class JsonProcessingOffers {
 
-    public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/svc_offer";
-        String user = "gl_pg_user";
-        String password = "g0r4c3_l0ty";
-        String filePath = "..\\data\\offers.json";
-        String sql = "INSERT INTO offers(id, transportID, hotelID, city_arrival, city_departure, date_start, date_end ) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?) " +
-                "ON CONFLICT (id) DO UPDATE SET " +
-                "transportID = EXCLUDED.transportID, " +
-                "hotelID = EXCLUDED.hotelID, " +
-                "city_arrival = EXCLUDED.city_arrival, " +
-                "city_departure = EXCLUDED.city_departure, " +
-                "date_start = EXCLUDED.date_start, " +
-                "date_end = EXCLUDED.date_end;";
-
-
-        processJsonData(url, filePath, sql, user, password);
-    }
-
-    public static void processJsonData(String url, String filePath, String sql, String user, String password) {
+    public static void processJsonData(String url, JsonNode offerArray, String sql, String user, String password) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            File jsonFile = new File(filePath);
-            JsonNode transportsArray = mapper.readTree(jsonFile);
-            System.out.println(transportsArray.toString());
-
 
             // Establish database connection
             Connection conn = DriverManager.getConnection(url, user, password);
@@ -44,12 +17,12 @@ public class JsonProcessingOffers {
 
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             System.out.println(preparedStatement);
-            System.out.println(transportsArray.isArray());
+            System.out.println(offerArray.isArray());
 
             // Loop through JSON array // id, transportID, hotelID, cityArrival, cityDeparture, dateStart, dateEnd
-            if (transportsArray.isArray()) {
+            if (offerArray.isArray()) {
                 System.out.println("początek pętli");
-                for (JsonNode transport : transportsArray) {
+                for (JsonNode transport : offerArray) {
                     preparedStatement.setLong(1, transport.get("id").longValue());
                     preparedStatement.setLong(2, transport.get("transportID").longValue());
                     preparedStatement.setLong(3, transport.get("hotelID").longValue());
---
+++ service-offer/src/main/java/com/goraceloty/offerservice/offer/config/StartupConfiguration.java
@@ -0,0 +1,57 @@
+package com.goraceloty.offerservice.offer.config;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goraceloty.offerservice.JsonProcessingOffers;
+
+
+@Configuration
+public class StartupConfiguration implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final ResourceLoader resourceLoader;
+    private final ObjectMapper objectMapper;
+
+    public StartupConfiguration(ResourceLoader resourceLoader, ObjectMapper objectMapper) {
+        this.resourceLoader = resourceLoader;
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public Runnable processJsonDataOnStartup() {
+        System.out.println("StartedInserting");
+        return () -> {
+            try {
+
+                var jsonFile = resourceLoader.getResource("file:data/offers.json").getInputStream();
+                System.out.println("Got path");
+                var jsonData = objectMapper.readTree(jsonFile);
+                System.out.println("Data loaded");
+                JsonProcessingOffers.processJsonData(
+                        "jdbc:postgresql://localhost:5432/svc_offer",
+                        jsonData,"INSERT INTO offers(id, transportID, hotelID, city_arrival, city_departure, date_start, date_end ) " +
+                                "VALUES (?, ?, ?, ?, ?, ?, ?) " +
+                                "ON CONFLICT (id) DO UPDATE SET " +
+                                "transportID = EXCLUDED.transportID, " +
+                                "hotelID = EXCLUDED.hotelID, " +
+                                "city_arrival = EXCLUDED.city_arrival, " +
+                                "city_departure = EXCLUDED.city_departure, " +
+                                "date_start = EXCLUDED.date_start, " +
+                                "date_end = EXCLUDED.date_end;",
+                        "gl_pg_user",
+                        "g0r4c3_l0ty");
+                System.out.println("Data loaded full");
+            } catch (Exception e) {
+                System.out.println("Error");
+                e.printStackTrace();
+            }
+        };
+    }
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        processJsonDataOnStartup().run();
+    }
+}
